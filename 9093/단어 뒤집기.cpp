4fc4551b51d7f4cde69cea4576c5c3cb5c2899a3
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-01-21 22:19:09 +0900

    solve problem 10828 9093

---
+++ 9093/단어 뒤집기.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	int tc;
+	cin >> tc;
+	cin.ignore(); // tc 다음의 개행문자 무시
+	string str;
+	stack<char> s; // 스택 사용법
+	while (tc--) {string str;
+		getline(cin, str);
+		str += '\n';
+		for (char ch : str) { // string 문자별로 훑기
+			if(ch == '\n' || ch == ' ') {
+				while (!s.empty()) {
+					cout << s.top();
+					s.pop();
+				}
+				cout << ch;
+			}
+			else {
+				s.push(ch);
+			}
+		}
+	}
+	return 0;
+}
